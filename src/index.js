17e792d16e21d1e121b6509b6ec14c7681b3de0d
dilasathapa <thapadilasa71@gmail.com>
2026-01-19 22:23:52 +0530

    closing all running apps

---
+++ src/index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const {execSync} = require('child_process');
+
+const EXECLUDED_APPS = ["System Settings", "Google Chrome", "Visual Studio Code", "Finder"];
+
+function getRunningApplications() {
+    const script = `
+        tell application "System Events"
+            get name of (processes where background only is false)
+        end tell
+    `;
+    const result = execSync(`osascript -e '${script}'`).toString().trim();
+    console.log("Running applications before exclusion:", result);
+    return result.split(', ').filter(app => !EXECLUDED_APPS.includes(app));
+}
+
+function quitApplications(apps, force){
+    try {
+        const command = force 
+        ? `osascript -e 'tell application "${apps}" to quit saving no'`
+        : `osascript -e 'tell application "${apps}" to quit'`;
+        execSync(command);
+        console.log(`Successfully quit applications: ${apps}`);
+    } catch (error) {
+        console.log(`Failed to quit applications: ${apps}. Error: ${error.message}`);
+    }
+}
+
+function main(){
+    console.log("Closing running applications...\n");
+
+    const apps = getRunningApplications();
+    if(apps.length === 0){
+        console.log("No running applications found.");
+        return;
+    }
+    console.log(`Found running applications: ${apps}\n`);
+    
+    apps.forEach(quitApplications);
+    console.log("\nAll possible applications processed!");
+}
+
+main();
